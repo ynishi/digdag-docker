e596c36eb1441baa7740496027fae1185418518e
Yutaka Nishimura <ytk.nishimura@gmail.com>
2019-08-17 13:59:12 +0900

    update example use docker image for task

--- example/run_digdag.sh
+++ example/run_digdag.sh
@@ -4,10 +4,10 @@ cd $(dirname $0)
 echo output is in project, tmp
 mkdir -p tmp
 chmod 777 tmp
-mkdir -p project 
+mkdir -p project
 chmod 777 project
 
 if [ "$1" = run ]; then
   out='-o /tmp'
 fi
-docker run --rm -it -v $(pwd)/project:/project -w /project -v $(pwd)/tmp:/tmp digdag digdag $@ $out 
+docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/project:/project -w /project -v $(pwd)/tmp:/tmp digdag digdag $@ $out
